9a967f624f7268ba75db6e7a3583bf0fc9d9bb1c
Alexandre Marcireau <alexandre.marcireau@gmail.com>
2020-07-16 21:15:21 +1000

    Adapt the bench tests to the API change

--- benches/propagate.rs
+++ benches/propagate.rs
@@ -9,9 +9,13 @@ pub fn criterion_benchmark(criterion: &mut Criterion) {
         b.iter(|| {
             let mut predictions = Vec::new();
             for test_case in test_cases.list.iter() {
-                let constants = sgp4::Constants::from_tle_afspc_compatibility_mode(
-                    &sgp4::tle::parse(None, test_case.line1.as_bytes(), test_case.line2.as_bytes())
-                        .unwrap(),
+                let constants = sgp4::Constants::from_elements_afspc_compatibility_mode(
+                    &sgp4::Elements::from_tle(
+                        None,
+                        test_case.line1.as_bytes(),
+                        test_case.line2.as_bytes(),
+                    )
+                    .unwrap(),
                 )
                 .unwrap();
                 for state in &test_case.states {
